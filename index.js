3d589e5d4373b745b4f48365d57575d852a14377
Christina Lena Korosec <christina.korosec@ds003.info>
2018-07-17 00:24:51 +0200

    Initial expression parser

--- index.js
+++ index.js
@@ -6,15 +6,227 @@ var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
 const client = new Discord.Client();
 const token = config.token;
+const commandPrefix = config.prefix;
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
+  if (msg.content.startsWith(commandPrefix)) {
+    let reply = executeCommand(msg.content.substr(1, msg.content.length - 1));
+    msg.reply(reply);
   }
 });
 
 client.login(token);
+
+function executeCommand(command) {
+  if(command.startsWith('roll')) {
+    return executeExpression(command.substr(4, command.length - 4));
+  }
+}
+
+function doRoll(expression) {
+  expression = expression.trim();
+  let result = [];
+
+  if(expression.length === 0) {
+    result.push(getResultObject(getRandom(20), true));
+    return result;
+  }
+  let expressions = expression.split(/(?=[+-])/);
+  for(let i = 0; i < expressions.length; i++) {
+    let current = expressions[i].trim().match(/([^d]*d[^ ]*[ ])(.*)/);
+    if(current === null) {
+      current = expressions[i].trim().match(/([^0-9]*[0-9]*)(.*)/);
+    }
+    let diceExp = current[1].trim();
+
+    let positive = true;
+    if(diceExp.startsWith('+')) {
+      positive = true;
+    }
+    else if(diceExp.startsWith('-')) {
+      positive = false;
+    }
+
+    if(diceExp.startsWith('+') || diceExp.startsWith('-')) {
+      diceExp = diceExp.substr(1, diceExp.length - 1).trim();
+    }
+
+    let name = '';
+    if(typeof current[2] !== 'undefined') {
+      name = current[2].trim();
+    }
+
+    if(diceExp.length === 0) {
+      throw `Invalid dice expression [${diceExp}]`;
+    }
+
+    if(diceExp.indexOf('d') < 0) {
+      if(isNaN(diceExp)) {
+        throw `Invalid modifier [${diceExp}]`;
+      }
+      result.push(getResultObject(Number(diceExp), positive, name));
+      continue;
+    }
+
+    let expressionParts = diceExp.split("d", 2);
+    let diceNum = 1;
+    if(expressionParts[0].trim().length !== 0) {
+      if(isNaN(expressionParts[0])) {
+        throw `Invalid dice count [${diceExp}]`;
+      }
+      else {
+        diceNum = Number(expressionParts[0].trim());
+      }
+    }
+    if(diceNum < 1 || diceNum > 200) {
+      throw `Invalid dice count [${diceExp}]`;
+    }
+
+    let dieSizeString = expressionParts[1];
+    if(typeof dieSizeString === 'undefined') {
+      throw `Invalid dice expression [${diceExp}]`;
+    }
+    dieSizeString = dieSizeString.trim();
+    if(dieSizeString.length === 0) {
+      throw `Invalid die size [${diceExp}]`;
+    }
+
+    let dieSize = Number(dieSizeString);
+
+    if(dieSize < 2 || dieSize >= 1000000) {
+      throw `Invalid die size [${diceExp}]`;
+    }
+
+    if(diceNum > 1) {
+      let diceResults = [];
+      for(let j = 0; j < diceNum; j++) {
+        diceResults[j] = getRandom(dieSize);
+      }
+      result.push(getResultObject(diceResults, positive, name));
+    }
+    else {
+      result.push(getResultObject(getRandom(dieSize), positive, name));
+    }
+  }
+  return result;
+}
+
+function getResultObject(value, positive, name) {
+  let total = 0;
+  if(typeof value === 'number') {
+    total = value;
+  }
+  else if(Array.isArray(value)) {
+    for(let i = 0; i < value.length; i++) {
+      total += value[i];
+    }
+  }
+  else {
+    throw 'Invalid value type';
+  }
+
+  if(typeof name === 'undefined' || !name) {
+    return {
+      total: total,
+      value: value,
+      positive: positive
+    };
+  }
+  return {
+    total: total,
+    name: name,
+    value: value,
+    positive: positive
+  };
+}
+
+function resultArrayToString(results) {
+  let text = '';
+  if(results.length === 1 && (typeof results[0].value === 'number' || (Array.isArray(results[0].value) && results[0].length === 1))) {
+    text = String(results[0].total);
+    
+    if(results[0].name) {
+      text += ' ' + results[0].name;
+    }
+
+    return text;
+  }
+
+  let total = 0;
+  for(let i = 0; i < results.length; i++) {
+    if(results[i].positive) {
+      total += results[i].total;
+    }
+    else {
+      total -= results[i].total;
+    }
+    text += resultToString(results[i]);
+    if(i < results.length - 1) {
+      text += ', ';
+    }
+  }
+  text = String(total) + ': ' + text;
+  return text;
+}
+
+function resultToString(result) {
+  let text = String(result.total);
+
+  if(!result.positive) {
+    text = '-' + text;
+  }
+
+  if(typeof result.value === 'number') {
+    // nop
+  }
+  else if(Array.isArray(result.value)) {
+    text += ' (';
+    for(let i = 0; i < result.value.length; i++) {
+      text += String(result.value[i]);
+      if(i < result.value.length - 1) {
+        text += ', ';
+      }
+    }
+    text += ')';
+  }
+  else {
+    throw 'Invalid result object';
+  }
+
+  if(result.name) {
+    text += ' ' + result.name;
+  }
+
+  return text;
+}
+
+function getRandom(max) {
+  return getRandomWithMin(1, max);
+}
+
+function getRandomWithMin(min, max) {
+  return Math.floor(min + (Math.random() * (max-min+1)));
+}
+
+function executeExpression(expression) {
+  return expression + ': ' + resultArrayToString(doRoll(expression));
+}
+
+/*let results = [];
+for(let i = 0; i < 1000; i++) {
+  let rand = getRandom(1, 20);
+  if(typeof results[rand] === 'undefined') {
+    results[rand] = 0;
+  }
+  results[rand] = results[rand] + 1;
+}
+
+for(let i = 0; i < results.length; i++) {
+  console.log(i + ": " + results[i]);
+}*/
+
+// console.log(executeExpression('3d8 cold + 1d6 bludgeoning dmg+3d4 piercing-1 STR mod'));
